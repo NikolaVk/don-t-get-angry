1682514543953116b6b801d7e36274c67aca3ec6
NikolaVk <nikola@ique.nl>
2021-08-08 22:18:50 +0200

    Getting layer element references in java

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,8 +1,28 @@
 
 
+let el_playfield        = null;
+let el_player           = null;
+let el_popup            = null;
 
 
-loadGame()
+
+function loadGame()
 {
-    alert('Loading game');
+   el_playfield     = document.getElementById('gamefield');
+   el_player        = document.getElementById('player');
+   el_popup         = document.getElementById('menu');
+   if ((el_playfield == null) || (el_player == null) || (el_popup == null))
+   {
+       showError("Internal error");
+       return false;
+   }
+
+   el_playfield.innerHTML         = "We have a playfield element";
+
 }
+
+
+function showError(ErrorMessage)
+    {
+        alert("Error: " + ErrorMessage);
+    }
